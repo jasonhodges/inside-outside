eeb9389c30946b3bd163271cdb346312a65f99fc
Jason Hodges <jasonlhodges@gmail.com>
2023-12-23 17:08:52 -0500

    Add Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import {Metadata} from "next";
 import 'global.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata: Metadata = {
   title: 'Home',
   description: 'Welcome to Next.js',
@@ -10,6 +11,7 @@ export default function RootLayout({children}: {
   return (
     <html lang="en">
       <body>{children}</body>
+    <SpeedInsights />
     </html>
   )
 }
